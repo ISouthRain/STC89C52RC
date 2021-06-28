e9ee2cf6366fdc42a847d39859d6507360e6853f
ISouthRain <gaozhongjie2@gmail.com>
2021-06-28 19:21:37 +0800

    登上顶点吧!

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include "8052.h"
+#define LED P1_0
+#define LED1 P1_3
+void main()
+{
+	LED1=1;
+	LED1=0;
+	LED=1;
+	LED=0;
+	while (1) {
+	}
+}
--- h_file/LED/ALotOfLed.h
+++ h_file/LED/ALotOfLed.h
@@ -1,6 +1,4 @@
 void MoreLed(int a)
 {
     P1=a;
-    while(1){
-    }
 }
