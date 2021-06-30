f4f2ec7d7f2d447d372fa072c70d33cce655cf0c
nanfengchuangyu2 <65887663+nanfengchuangyu2@users.noreply.github.com>
2021-06-30 17:15:18 +0800

    登上顶点吧！

--- 1.c
+++ 1.c
@@ -1,21 +1,6 @@
-#include "8052.h"
-#include "LED.h"
-#include "Delay.h"
-void main()
-{
-		unsigned char i;
-		Delay(500000);
-		LED_P1(0x7f);
-		for(i=0;i<8;i++){
-				Delay(500000);
-				P1>>=1;
-		}
-		while(1){
-            Delay(500000);
-            LED_P1(0xf7);
-		for(i=0;i<8;i++){
-				Delay(500000);
-				P1>>=1;
-		}
-		}
+#include"8052.h"
+#include"LED.h"
+#include"Delay.h"
+void main() {
+		P1=0x7f;
 }
